a1dba233f289ec6cea10639cb386715ee7c9c069
Kim Hamper <kim.hamper@gmail.com>
2013-11-25 10:24:18 -0500

    Add ability to add goals to all categories

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	var storage_key = 'goalslist';
+	var goals = {},
+		settings = {};
 
 	// function for testing is a string is blank
 	function isBlank(str) {
@@ -8,10 +10,13 @@ $(document).ready(function(){
 
 	// display goals entered previously
 	if (localStorage && storage_key in localStorage){
-		var stored_goals = localStorage.getItem(storage_key);
+		var stored_goals = JSON.parse(localStorage.getItem(storage_key));
 
-		if (stored_goals !== 'undefined'){
-			$('#goals').html(stored_goals);
+		if (stored_goals){
+			for (key in stored_goals){
+				(stored_goals[key] && !isBlank(stored_goals[key]) && stored_goals[key] !== "undefined") ? 
+					$('#' + key + ' .goals').html(stored_goals[key]) : "";
+			}
 		}
 
 		getSettings();
@@ -19,43 +24,46 @@ $(document).ready(function(){
 	}
 
 	// enable click outside to create new goal
-	$('#new-goal').focusout(function(){
-		createGoal(this);
+	$('.new-goal').focusout(function(){
+		createGoal(this, $(this).parent());
 	});
 
 	// enable press enter to create new goal
-	$('#new-goal').keyup(function(e){
+	$('.new-goal').keyup(function(e){
 		if(e.keyCode == 13){
-			createGoal(this);
+			createGoal(this, $(this).parent());
 		}	
 	});
 
-	function createGoal(input_node){
+	function createGoal(input_node, parent_node){
 		var new_goal = $(input_node).val();
 
 		if (!isBlank(new_goal)){
 			// add the goal to the list
 			var delete_button = '<a href="" class="delete">x</a>'
-			$('#goals').append('<div class="goal"><h3>' + new_goal + '</h3>' + delete_button + '</div>');
+			var parent = $(parent_node).attr('id');
+			$('#' + parent + ' .goals').append('<div class="goal"><h3>' + new_goal + '</h3>' + delete_button + '</div>');
 
 			// clear the text input box
 			$(input_node).val("");
 
-			setStorage(storage_key, '#goals');
+			goals = {
+							'cat-section-1': $('#cat-section-1 .goals').html(),
+							'cat-section-2': $('#cat-section-2 .goals').html(),
+							'cat-section-3': $('#cat-section-3 .goals').html(),
+							'cat-section-4': $('#cat-section-4 .goals').html()
+						}
+
+			localStorage.setItem(storage_key, JSON.stringify(goals));
 		}
 	}
 
 	$('.delete').click(function(){
 		$(this).parent().remove();
-		setStorage(storage_key, '#goals');
+		setStorage[storage_key] = JSON.stringify(goals);
 	});
 
-	function setStorage(key, selector){
-		localStorage.setItem(key, $(selector).html());
-	}
-
 	/* APPLY SETTINGS --------------------------------------------------------------- */
-	var settings;
 
 	// show-hide settings form
 	$('#settings-toggle').click(function(){
